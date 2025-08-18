1fa9ab6a4dd8bf05e89513a89c59e99ff1ceceb8
Fae <faehupp@gmail.com>
2025-08-18 12:09:35 -0500

    owo

--- protos/protogen.py
+++ protos/protogen.py
@@ -2,33 +2,30 @@
 
 rpcs: set[str] = set()
 
-def main():
-    name = input("--- ")
+name = input("--- ")
 
 
+j = input(">>> ")
+while j not in [""]:
+    rpcs.add(j.capitalize())
     j = input(">>> ")
-    while j not in [""]:
-        rpcs.add(j.capitalize())
-        j = input(">>> ")
-    
-    f = open("out.proto", mode= "+w")
 
-    product = ""
+f = open("out.proto", mode= "+w")
 
-    product += 'syntax = "proto3";\n'
-    product += f'package {name}service;\n\n'
-    product += f'service {name.capitalize()}' + " {\n"
-    for rpc in rpcs:
-        product += f"    rpc {rpc} ({rpc}Request) returns ({rpc}Response);\n"
+product = ""
+
+product += 'syntax = "proto3";\n'
+product += f'package {name}service;\n\n'
+product += f'service {name.capitalize()}' + " {\n"
+for rpc in rpcs:
+    product += f"    rpc {rpc} ({rpc}Request) returns ({rpc}Response);\n"
+product += "}\n\n"
+
+for rpc in rpcs:
+    product += f"message {rpc}Request" + " {\n"
+    product += "}\n"
+    product += f"message {rpc}Response" + " {\n"
     product += "}\n\n"
 
-    for rpc in rpcs:
-        product += f"message {rpc}Request" + " {\n"
-        product += "}\n"
-        product += f"message {rpc}Response" + " {\n"
-        product += "}\n\n"
+print(product, file=f)
 
-    print(product, file=f)
-
-if __name__ == "__main__":
-    main()
